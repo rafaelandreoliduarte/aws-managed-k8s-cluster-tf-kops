d6b100424a684e8e559c27b9c7eac30548681af3
Rafael Andreoli Duarte <rafael.duarte@saber.com.br>
2022-04-04 22:30:25 -0300

    nova branch

--- terraform/deployment/backend.tf
+++ terraform/deployment/backend.tf
@@ -7,3 +7,15 @@ terraform {
     dynamodb_table = "terraform-state-lock"
   }
 }
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "database_cluster"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "bar"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
